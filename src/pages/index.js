a1ca17979ad04dc1b3016efb2604d80eeeaede9e
Matt <matt@matthughes.dev>
2020-06-15 21:29:47 -0700

    favicon and font size

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,23 @@ import React from "react"
 import "../styles/layout.css"
 import Container from "../components/Container.jsx"
 import ReactGA from "react-ga"
+import { Helmet } from "react-helmet"
+import Me from "../images/me.jpg"
 
 function initializeReactGA() {
   ReactGA.initialize("UA-160538981-1")
   ReactGA.pageview("/homepage")
 }
 initializeReactGA()
-const IndexPage = () => <Container />
+const IndexPage = () => (
+  <div>
+    <Helmet>
+      <link rel="icon" href={Me} />
+      <meta charSet="utf-8" />
+      <title>Matt Hughes</title>
+    </Helmet>
+    <Container />
+  </div>
+)
 
 export default IndexPage
